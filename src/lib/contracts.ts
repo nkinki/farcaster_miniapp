024a8064270f83907d5393398880cdf106263d15
nkinki <nkinki2014@gmail.com>
2025-08-04 18:15:18 +0200

    feat: Implement Farcaster Frame structure with two buttons

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -1,5 +1,5 @@
 // CSERÉLD KI EZEKET A HELYES CÍMEKRE A BASE HÁLÓZATON!
-export const PROMO_CONTRACT_ADDRESS = '0x6d41927f5707965017be491d98b28a3c2a52164f'; // A FarcasterPromo contractod címe
+export const PROMO_CONTRACT_ADDRESS = '0xeca8a11700476863a976b841dc32e351acf6ed1f'; // A FarcasterPromo contractod címe
 export const CHESS_TOKEN_ADDRESS = '0x47AF6bd390D03E266EB87cAb81Aa6988B65d5B07';    // A CHESS token contract címe
 
 // Importáld az ABI-kat a JSON fájlokból
